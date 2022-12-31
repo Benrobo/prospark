dd89183255dc3d00e4b5c0cbf33be33a35dd161a
benrobo <alumonabenaiah71@gmail.com>
2022-12-31 12:35:57 -0800

    updated svelte template

--- cli/src/data/template.ts
+++ cli/src/data/template.ts
@@ -173,3 +173,19 @@ export const COUNTER_SVELTE = `
   count is {count}
 </button>
 `
+
+export const SVELTE_INDEX_HTML = `
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <link rel="icon" type="image/svg+xml" href="/vite.svg" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>{{title}}</title>
+  </head>
+  <body>
+    <div id="app"></div>
+    <script type="module" src="{{script_link}}"></script>
+  </body>
+</html>
+`
